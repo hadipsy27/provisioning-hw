b65f0e9cfdae71ce204382dbea6d78ff8cbf7842
900656 <hadi.prasetyo900656@bni.co.id>
2025-07-01 14:46:50 +0700

    Initial Project

---
+++ src/main/java/com/voxloud/provisioning/controller/ProvisioningController.java
@@ -0,0 +1,11 @@
+package com.voxloud.provisioning.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ProvisioningController {
+
+    // TODO Implement controller method
+}
---
+++ src/main/java/com/voxloud/provisioning/service/ProvisioningServiceImpl.java
@@ -0,0 +1,12 @@
+package com.voxloud.provisioning.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProvisioningServiceImpl implements ProvisioningService {
+
+    public String getProvisioningFile(String macAddress) {
+        // TODO Implement provisioning
+        return null;
+    }
+}
